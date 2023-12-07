f892031d38a0e399f112b2f12497105813b53b5a
Glessme <roger02michel@outlook.fr>
2023-12-07 12:19:46 +0100

    8emecommit

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{config('app.basicfit') }} - @yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <a href="{{ route('main.accueil') }}">Accueil</a>
+            <a href="{{ route('main.cours') }}">Cours</a>
+            <a href="{{ route('main.tarifs') }}">Tarifs</a>
+            <a href="{{ route('main.contact') }}">Contact</a>
+            <a href="{{ route('main.mentionslegales') }}">Mentions Legales</a>      
+        </nav>
+    </header>
+    @yield('content')
+    <footer>
+        <a href="{{ route('main.mentionslegales') }}">Mentions Legales</a>      
+    </footer>
 </body>
 </html>
